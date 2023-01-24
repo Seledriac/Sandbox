1653da117e4d1079b0ff39786fb347faa321327d
MP-SCHMIDT <96049257+MP-SCHMIDT@users.noreply.github.com>
2023-01-24 17:47:57 +0100

    Rudimentary version of koch snowflake

--- FractalCurveDevelopment.cpp
+++ FractalCurveDevelopment.cpp
@@ -30,23 +30,27 @@ void FractalCurveDevelopment::Init() {
   Nodes.clear();
 
   int maxDepth= int(std::round(D.param[testVar0____________].val));
-  if (maxDepth <= 0) return;
+  if (maxDepth < 1) return;
 
-  Nodes.resize(maxDepth);
+  Nodes.push_back(std::vector<Math::Vec3>());
   Nodes[0].push_back(Math::Vec3(0.0, 0.0, 0.0));
   Nodes[0].push_back(Math::Vec3(0.0, 1.0, 0.0));
 
-  for (unsigned int idxDepth= 1; idxDepth < Nodes.size(); idxDepth++) {
-    for (unsigned int idxNode= 0; idxNode < Nodes[idxDepth - 1].size() - 1; idxNode++) {
+  for (int idxDepth= 1; idxDepth < maxDepth; idxDepth++) {
+    Nodes.push_back(std::vector<Math::Vec3>());
+    for (int idxNode= 0; idxNode < int(Nodes[idxDepth - 1].size()) - 1; idxNode++) {
       Math::Vec3 posA= Nodes[idxDepth - 1][idxNode];
       Math::Vec3 posB= Nodes[idxDepth - 1][idxNode + 1];
 
+      Math::Vec3 posM= (posA + posB) / 2.0 + 0.4 * (posB - posA).norm() * (posB - posA).cross(Math::Vec3(0.0, 0.0, 1.0)).normalized();
+
       Nodes[idxDepth].push_back(Math::Vec3(0.0, 0.0, 0.1) + posA);
       Nodes[idxDepth].push_back(Math::Vec3(0.0, 0.0, 0.1) + posA + (posB - posA) * 0.25);
-      Nodes[idxDepth].push_back(Math::Vec3(0.0, 0.0, 0.1) + posA + (posB - posA) * 0.50 + Math::Vec3(0.3, 0.0, 0.0));
+      // Nodes[idxDepth].push_back(Math::Vec3(0.0, 0.0, 0.1) + posA + (posB - posA) * 0.50 + Math::Vec3(0.3, 0.0, 0.0));
+      Nodes[idxDepth].push_back(Math::Vec3(0.0, 0.0, 0.1) + posM);
       Nodes[idxDepth].push_back(Math::Vec3(0.0, 0.0, 0.1) + posA + (posB - posA) * 0.75);
 
-      if (idxNode == Nodes[idxDepth - 1].size() - 2) {
+      if (idxNode == int(Nodes[idxDepth - 1].size()) - 2) {
         Nodes[idxDepth].push_back(Math::Vec3(0.0, 0.0, 0.1) + posB);
       }
     }
@@ -60,11 +64,11 @@ void FractalCurveDevelopment::Draw() {
   if (D.showNodes) {
     glPointSize(3.0f);
     glBegin(GL_POINTS);
-    for (unsigned int idxDepth= 0; idxDepth < Nodes.size(); idxDepth++) {
+    for (int idxDepth= 0; idxDepth < int(Nodes.size()); idxDepth++) {
       double r, g, b;
       SrtColormap::RatioToJetBrightSmooth(double(idxDepth) / double(Nodes.size() - 1), r, g, b);
       glColor3d(r, g, b);
-      for (unsigned int idxNode= 0; idxNode < Nodes[idxDepth].size(); idxNode++) {
+      for (int idxNode= 0; idxNode < int(Nodes[idxDepth].size()); idxNode++) {
         glVertex3dv(Nodes[idxDepth][idxNode].array());
       }
     }
@@ -74,12 +78,12 @@ void FractalCurveDevelopment::Draw() {
 
   if (D.showEdges) {
     glLineWidth(3.0f);
-    for (unsigned int idxDepth= 0; idxDepth < Nodes.size(); idxDepth++) {
+    for (int idxDepth= 0; idxDepth < int(Nodes.size()); idxDepth++) {
       double r, g, b;
       SrtColormap::RatioToJetBrightSmooth(double(idxDepth) / double(Nodes.size() - 1), r, g, b);
       glBegin(GL_LINE_STRIP);
       glColor3d(r, g, b);
-      for (unsigned int idxNode= 0; idxNode < Nodes[idxDepth].size(); idxNode++) {
+      for (int idxNode= 0; idxNode < int(Nodes[idxDepth].size()); idxNode++) {
         glVertex3dv(Nodes[idxDepth][idxNode].array());
       }
       glEnd();
