069393e9e1f342b66c7aefa08cbf668cecc174de
MP-SCHMIDT <96049257+MP-SCHMIDT@users.noreply.github.com>
2023-01-25 18:27:06 +0100

    fixes on fractal curves codes

--- FractalCurveDevelopment.cpp
+++ FractalCurveDevelopment.cpp
@@ -20,14 +20,6 @@
 extern Data D;
 
 
-// std::vector<Math::Vec3> KochNewNodes(Math::Vec3 iPosA, Math::Vec3 iPosB) {
-//   std::vector<Math::Vec3> newNodes(3);
-//   newNodes[0]= iPosA + (iPosB - iPosA) * 1.0 / 3.0;
-//   newNodes[1]= (iPosA + iPosB) / 2.0 + (std::sqrt(3.0) / 6.0) * (iPosB - iPosA).norm() * (iPosB - iPosA).cross(Math::Vec3(0.0, 0.0, 1.0)).normalized();
-//   newNodes[2]= iPosA + (iPosB - iPosA) * 2.0 / 3.0;
-//   return newNodes;
-// }
-
 FractalCurveDevelopment::FractalCurveDevelopment() {
   isInitialized= false;
   Nodes.clear();
@@ -36,20 +28,17 @@ FractalCurveDevelopment::FractalCurveDevelopment() {
 
 void FractalCurveDevelopment::Init() {
   int maxDepth= int(std::round(D.param[testVar0____________].val));
-  if (maxDepth < 1) return;
-
-  // TODO fix crash when recursion = 1 ?
+  if (maxDepth < 2) return;
 
   Faces.clear();
   Nodes.clear();
 
   // Initialize the curve at depth 0
   Nodes.resize(1, std::vector<Math::Vec3>(4));
-  Nodes[0][0]= Math::Vec3(0.0, -1.0, 0.0);
-  Nodes[0][1]= Math::Vec3(std::sqrt(3.0), 0.0, 0.0);
-  Nodes[0][2]= Math::Vec3(0.0, 1.0, 0.0);
-  Nodes[0][3]= Math::Vec3(0.0, -1.0, 0.0);
-  // TODO fix node duplication issue
+  Nodes[0][0]= Math::Vec3(-std::sqrt(3.0) / 2.0, -1.0, 0.0);
+  Nodes[0][1]= Math::Vec3(std::sqrt(3.0) / 2.0, 0.0, 0.0);
+  Nodes[0][2]= Math::Vec3(-std::sqrt(3.0) / 2.0, 1.0, 0.0);
+  Nodes[0][3]= Math::Vec3(-std::sqrt(3.0) / 2.0, -1.0, 0.0);
 
   // Iteratively build the next level in the fractal recursion depth
   for (int idxDepth= 1; idxDepth < maxDepth; idxDepth++) {
@@ -57,50 +46,28 @@ void FractalCurveDevelopment::Init() {
     for (int idxNode= 0; idxNode < int(Nodes[idxDepth - 1].size()) - 1; idxNode++) {
       Math::Vec3 posA= Nodes[idxDepth - 1][idxNode];
       Math::Vec3 posB= Nodes[idxDepth - 1][idxNode + 1];
-      Math::Vec3 ZOffset(0.0, 0.0, 0.6 / std::pow(1.5, double(idxDepth)));
+      Math::Vec3 ZOffset(0.0, 0.0, -0.5 / std::pow(1.25, double(idxDepth)));
 
-      // TODO fix node duplication issue
       Math::Vec3 posN0= ZOffset + posA;
       Math::Vec3 posN1= ZOffset + posA + (posB - posA) * 1.0 / 3.0;
       Math::Vec3 posN2= ZOffset + (posA + posB) / 2.0 + (std::sqrt(3.0) / 6.0) * (posB - posA).norm() * (posB - posA).cross(Math::Vec3(0.0, 0.0, 1.0)).normalized();
       Math::Vec3 posN3= ZOffset + posA + (posB - posA) * 2.0 / 3.0;
       Math::Vec3 posN4= ZOffset + posB;
 
-      // TODO fix node duplication issue
-      Nodes[idxDepth].push_back(posN0);
+      if (idxNode == 0)
+        Nodes[idxDepth].push_back(posN0);
       Nodes[idxDepth].push_back(posN1);
       Nodes[idxDepth].push_back(posN2);
       Nodes[idxDepth].push_back(posN3);
       Nodes[idxDepth].push_back(posN4);
 
       Math::Vec3 posM= (posA + posB) / 2.0;
-      Faces.push_back({posM, posB, posN4});
-      Faces.push_back({posM, posN4, posN3});
-      Faces.push_back({posM, posN3, posN2});
-      Faces.push_back({posM, posN2, posN1});
-      Faces.push_back({posM, posN1, posN0});
-      Faces.push_back({posM, posN0, posA});
-
-      // TODO cleanup code
-
-      // std::vector<Math::Vec3> newNodes= KochNewNodes(posA, posB);
-
-      // if (idxNode == 0)
-      //   Nodes[idxDepth].push_back(ZOffset + posA);
-      // // for (auto newNode : newNodes)
-      // //   Nodes[idxDepth].push_back(newNode + ZOffset);
-      // Nodes[idxDepth].push_back(ZOffset + posA + (posB - posA) * 1.0 / 3.0);
-      // Nodes[idxDepth].push_back(ZOffset + (posA + posB) / 2.0 + (std::sqrt(3.0) / 6.0) * (posB - posA).norm() * (posB - posA).cross(Math::Vec3(0.0, 0.0, 1.0)).normalized());
-      // Nodes[idxDepth].push_back(ZOffset + posA + (posB - posA) * 2.0 / 3.0);
-      // Nodes[idxDepth].push_back(ZOffset + posB);
-
-      // Math::Vec3 posM= (posA + posB) / 2.0;
-      // Faces.push_back({posM, posA, posA + ZOffset});
-      // Faces.push_back({posM, posA + ZOffset, newNodes[0] + ZOffset});
-      // Faces.push_back({posM, newNodes[0] + ZOffset, newNodes[1] + ZOffset});
-      // Faces.push_back({posM, newNodes[1] + ZOffset, newNodes[2] + ZOffset});
-      // Faces.push_back({posM, newNodes[2] + ZOffset, posB + ZOffset});
-      // Faces.push_back({posM, posB + ZOffset, posB});
+      Faces.push_back({posM, posA, posN0});
+      Faces.push_back({posM, posN0, posN1});
+      Faces.push_back({posM, posN1, posN2});
+      Faces.push_back({posM, posN2, posN3});
+      Faces.push_back({posM, posN3, posN4});
+      Faces.push_back({posM, posN4, posB});
     }
   }
 
