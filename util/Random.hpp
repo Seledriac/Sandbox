04c6a31943a9f5f63cc3c5d07bbe2c6d3a1df2f1
MP-SCHMIDT <96049257+MP-SCHMIDT@users.noreply.github.com>
2023-09-08 17:19:58 +0200

    Util added double rand

--- util/Random.hpp
+++ util/Random.hpp
@@ -10,6 +10,10 @@ namespace Random {
     return iMin + (iMax - iMin) * (float(rand()) / float(RAND_MAX));
   }
 
+  inline double Val(double const iMin, double const iMax) {
+    return iMin + (iMax - iMin) * (double(rand()) / double(RAND_MAX));
+  }
+
   inline int Val(int const iMin, int const iMax) {
     if (iMax <= iMin) return iMin;
     return iMin + rand() % (iMax - iMin + 1);
