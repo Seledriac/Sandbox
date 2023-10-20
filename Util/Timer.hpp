43a8ed1f2ba8666053243f17db1e97081a916698
MP-SCHMIDT <96049257+MP-SCHMIDT@users.noreply.github.com>
2023-10-20 13:41:24 +0200

    Implemented timer stack

--- Util/Timer.hpp
+++ Util/Timer.hpp
@@ -2,15 +2,22 @@
 
 // Standard lib
 #include <chrono>
+#include <vector>
 
+std::vector<std::chrono::high_resolution_clock::time_point> TimerStack;
 
 namespace Timer {
-  inline double GetElapsedTime() {
-    static std::chrono::high_resolution_clock::time_point timeOld= std::chrono::high_resolution_clock::now();
-    static std::chrono::high_resolution_clock::time_point timeNew= std::chrono::high_resolution_clock::now();
-    timeNew= std::chrono::high_resolution_clock::now();
+  inline int PushTimer() {
+    TimerStack.push_back(std::chrono::high_resolution_clock::now());
+    return (int)TimerStack.size();
+  }
+
+  inline double PopTimer() {
+    if (TimerStack.empty()) return -1.0;
+    std::chrono::high_resolution_clock::time_point timeNew= std::chrono::high_resolution_clock::now();
+    std::chrono::high_resolution_clock::time_point timeOld= TimerStack[TimerStack.size() - 1];
     double elapsed= std::chrono::duration<double>(timeNew - timeOld).count();
-    timeOld= std::chrono::high_resolution_clock::now();
+    TimerStack.pop_back();
     return elapsed;
   }
 }  // namespace Timer
